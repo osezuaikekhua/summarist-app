a4f880d2f1229439700e9167b06d849e98ca813e
Osezua Ikekhua <osezuaikekhua@gmail.com>
2024-04-08 23:37:00 -0500

    Fixing API key error

--- src/FireBaseConfig.js
+++ src/FireBaseConfig.js
@@ -6,13 +6,13 @@ import {getFirestore} from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC9DtkJqeeD6Yzc4MdJrNPwflVT4iQv_3Q",
+  authDomain: "summarist-app-d1be9.firebaseapp.com",
+  projectId: "summarist-app-d1be9",
+  storageBucket: "summarist-app-d1be9.appspot.com",
+  messagingSenderId: "745978118348",
+  appId: "1:745978118348:web:0e141237306488c1c2c269",
+  measurementId: "G-W06YLZ9EYL"
 };
 
 // Initialize Firebase
